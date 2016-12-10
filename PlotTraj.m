7f879742872e3779e4d701fff078bb32a6236e69
Andrea Battaglia <andrea.battaglia@epfl.ch>
2016-12-10 16:07:44 +0100

    Première simulation
    J'ai essayé la première simulation: j'obtient une trajectoire
    exponentielle divergente, ce qui est clairement faux

---
+++ PlotTraj.m
@@ -0,0 +1,25 @@
+function PlotTraj(out)
+
+output = load(out);
+t = output(:,1);
+x1 = output(:,2);
+y1 = output(:,3);
+x2 = output(:,4);
+y2 = output(:,5);
+x3 = output(:,6);
+y3 = output(:,7);
+vx1 = output(:,8);
+vy1 = output(:,9);
+vx2 = output(:,10);
+vy2 = output(:,11);
+vx3 = output(:,12);
+vy3 = output(:,13);
+
+clear output
+
+figure
+plot(x1, y1, x2, y2)
+xlabel('x [m]')
+ylabel('y [m]')
+
+end
